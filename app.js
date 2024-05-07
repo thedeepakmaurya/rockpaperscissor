7b18b8320cd55ba840f59b554357538693444215
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-05-07 16:06:14 +0530

    score added

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll('.choice');
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score")
+const compScorePara = document.querySelector("#comp-score")
 
 const genCompChoices = () => {
     const options = ["rock", "paper", "scissors"];
@@ -18,14 +22,18 @@ const drawGame = () => {
 
 
 
-const showWinner = (userWin) => {
+const showWinner = (userWin, userChoice, compChoice) => {
     if (userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
         console.log("you win")
-        msg.innerText = "You win!"
+        msg.innerText = `You win! ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green"
     } else {
+        compScore++;
+        compScorePara.innerText = compScore;
         console.log("you lose")
-        msg.innerText = "You lose!"
+        msg.innerText = `You lose! ${compChoice} beats your ${userChoice}`;
         msg.style.backgroundColor = "red"
     }
 }
@@ -48,7 +56,7 @@ const playGame = (userChoice) => {
         } else {
             compChoice === "rock" ? false : true;
         }
-        showWinner(userWin);
+        showWinner(userWin, userChoice, compChoice);
     }
 }
 
